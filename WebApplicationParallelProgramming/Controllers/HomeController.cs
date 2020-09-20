31b68899faf33d5fd986a299b5c4ffcb626bee24
ASUS-LAPTOP\Jun <junallan@telus.net>
2020-09-20 12:05:05 -0600

    Demonstration of task library for parallel processing of 3 independant sleep tasks.

---
+++ WebApplicationParallelProgramming/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationParallelProgramming.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var postTask = Task<string>.Factory.StartNew(GetStatsByPost);
+            var trafficTask = Task<string>.Factory.StartNew(GetStatsByTrafficSource);
+            var audienceTask = Task<string>.Factory.StartNew(GetStatsByAudience);
+
+            //GetStatsByPost();
+            //GetStatsByTrafficSource();
+            //GetStatsByAudience();
+
+            await Task.WhenAll(postTask, trafficTask, audienceTask);
+            stopwatch.Stop();
+
+            //ViewData["result"] = $"Total time taken for processing the request in seconds: {stopwatch.Elapsed.Seconds}";
+            ViewData["result"] = postTask.Result + "<br/>" + trafficTask.Result + "<br/>" + audienceTask.Result
+                + "<br/>" + "<br/>"
+                + "Total time taken for processing the request in seconds : " + stopwatch.Elapsed.Seconds;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public string GetStatsByPost()
+        {
+            Thread.Sleep(5000);
+            return "Thread Id: " + Thread.CurrentThread.ManagedThreadId.ToString() + "<br/>"
+                + "Finsihed getting stats by post at" + DateTime.Now.ToString();
+        }
+        public string GetStatsByTrafficSource()
+        {
+            Thread.Sleep(3000);
+            return "Thread Id: " + Thread.CurrentThread.ManagedThreadId.ToString() + "<br/>"
+                + "Finsihed getting stats by post at" + DateTime.Now.ToString();
+        }
+        public string GetStatsByAudience()
+        {
+            Thread.Sleep(2000);
+            return "Thread Id: " + Thread.CurrentThread.ManagedThreadId.ToString() + "<br/>"
+                  + "Finsihed getting stats by post at" + DateTime.Now.ToString();
+        }
+    }
+}
