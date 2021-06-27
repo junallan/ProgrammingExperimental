6ffc13230f388a120a56866dabc42aa44ae2d691
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-06-27 14:05:32 -0600

    Current conditions display implementation of observer pattern

---
+++ HeadFirstDesignPatterns/ObserverPattern/IObserver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern
+{
+    public interface IObserver
+    {
+        public void Update(float temp, float humidity, float pressure);
+    }
+}
