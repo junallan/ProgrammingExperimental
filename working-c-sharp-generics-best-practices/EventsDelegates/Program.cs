572d0a19b4d139dfd02d4371db3c67d10f5aecd5
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-09 08:38:15 -0600

    Working with Generic Events and Delegates

---
+++ working-c-sharp-generics-best-practices/EventsDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EventsDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generic Events and Delegates");
+
+            var authorRepo = new Repository<Author>();
+            authorRepo.EntityAdded += Repo_EntityAdded;
+            authorRepo.Add(new Author("Jun", "Parreno"));
+
+           
+        }
+
+        private static void Repo_EntityAdded(object sender, EntityAddedEventArgs<Author> args)
+    }
+
+    public record Author(string FirstName, string LastName);
+    public record Course(string CourseName);
+}
---
+++ working-c-sharp-generics-best-practices/EventsDelegates/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegates
+{
+    public interface IWriteRepository<T>
+    {
+        void Add(T entity);
+        event EventHandler<T> EntityAdded;
+    }
+
+    public interface IReadRepository<T>
+    {
+        IEnumerable<T> List();
+    }
+
+    public class Repository<T> : IWriteRepository<T>//, IReadRepository<T>
+    {
+        private static List<T> _data = new();
+
+        public IEnumerable<T> List()
+        {
+            return _data.AsEnumerable();
+        }
+
+        public void Add(T entity)
+        {
+            _data.Add(entity);
+            OnEntityAdded(entity);
+        }
+
+        public event EventHandler<T> EntityAdded;
+
+        protected virtual void OnEntityAdded(T entityAdded)
+        {
+            EntityAdded?.Invoke(this, entityAdded);
+        }
+
+       
+    }
+}
