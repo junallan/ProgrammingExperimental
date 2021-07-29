6c58099cc6369fa6773164dd0d403347a3adf047
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-29 08:51:11 -0600

    Chain of Responsibility: A Chain of Handlers

--- c-sharp-design-patterns-chain-responsibility/Demo 1 - Chain of Responsibility First Look/Start_Here/Business/UserProcessor.cs
+++ c-sharp-design-patterns-chain-responsibility/Demo 1 - Chain of Responsibility First Look/Start_Here/Business/UserProcessor.cs
@@ -1,35 +1,51 @@
-﻿using Chain_of_Responsibility_First_Look.Business.Models;
+﻿using Chain_of_Responsibility_First_Look.Business.Exceptions;
+using Chain_of_Responsibility_First_Look.Business.Handlers.UserValidation;
+using Chain_of_Responsibility_First_Look.Business.Models;
 using Chain_of_Responsibility_First_Look.Business.Validators;
 
 namespace Chain_of_Responsibility_First_Look.Business
 {
     public class UserProcessor
     {
-        private SocialSecurityNumberValidator socialSecurityNumberValidator
-            = new SocialSecurityNumberValidator();
-
         public bool Register(User user)
         {
-            if (!socialSecurityNumberValidator.Validate(user.SocialSecurityNumber, user.CitizenshipRegion))
+            try
+            {
+                var handler = new SocialSecurityNumberValidatorHandler();
+                handler.SetNext(new AgeValidationHandler())
+                       .SetNext(new NameValidationHandler())
+                       .SetNext(new CitizenshipRegionValidationHandler());
+
+                handler.Handle(user);
+            }
+            catch (UserValidationException ex)
             {
                 return false;
             }
-            else if (user.Age < 18)
-            {
-                return false;
-            }
-            else if (user.Name.Length <= 1)
-            {
-                return false;
-            }
-            else if (user.CitizenshipRegion.TwoLetterISORegionName == "NO")
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
+
+            return true;
+
+
+            //if (!socialSecurityNumberValidator.Validate(user.SocialSecurityNumber, user.CitizenshipRegion))
+            //{
+            //    return false;
+            //}
+            //else if (user.Age < 18)
+            //{
+            //    return false;
+            //}
+            //else if (user.Name.Length <= 1)
+            //{
+            //    return false;
+            //}
+            //else if (user.CitizenshipRegion.TwoLetterISORegionName == "NO")
+            //{
+            //    return false;
+            //}
+            //else
+            //{
+            //    return true;
+            //}
         }
     }
 }
