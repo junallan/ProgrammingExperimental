2d99e3ef007f7199f92554f6b941ec6d1553d91a
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-09-18 13:35:55 -0600

    Composite builder pattern

--- c-sharp-design-patterns-composite/CompositeDemo/Program.cs
+++ c-sharp-design-patterns-composite/CompositeDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CompositeDemo.Structural;
+using Newtonsoft.Json;
 using System;
 
 namespace CompositeDemo
@@ -6,6 +7,24 @@ namespace CompositeDemo
     class Program
     {
         static void Main(string[] args)
+        {
+            var builder = new FileSystemBuilder("development");
+            builder.AddDirectory("project1");
+            builder.AddFile("p1f1.txt", 2100);
+            builder.AddFile("p1f2.txt", 3100);
+            builder.AddDirectory("sub-dir");
+            builder.AddFile("p1f3.txt", 4100);
+            builder.AddFile("p1f4.txt", 5100);
+            builder.SetCurrentDirectory("development");
+            builder.AddDirectory("project2");
+            builder.AddFile("p2f1.txt", 6100);
+            builder.AddFile("p2f2.txt", 7100);
+
+            Console.WriteLine($"Total size (root) : {builder.Root.GetSizeInKB()}");
+            Console.WriteLine(JsonConvert.SerializeObject(builder.Root, Formatting.Indented));
+        }
+
+        private static void FileSystemComposite()
         {
             var root = new DirectoryItem("development");
             var proj1 = new DirectoryItem("project1");
