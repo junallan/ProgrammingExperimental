769bb32682f1409c8a34b03c554fc3d4cdeda1cb
Jun Allan Parreno <junallan@telus.net>
2022-05-07 16:14:31 -0600

    Les Jackson REST API Course complete Read and Create endpoints

---
+++ lesJackson/S3/E2/Commander/Controllers/CommandsController.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using AutoMapper;
+using Commander.Data;
+using Commander.Dtos;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
+        {
+          _repository = repository;  
+          _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult <IEnumerable<CommandReadDto>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
+        }
+
+        [HttpGet("{id}", Name="GetCommandById")]
+        public ActionResult <CommandReadDto> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            if(commandItem != null)
+            {
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult <CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto)
+        {
+            var commandModel = _mapper.Map<Command>(commandCreateDto);
+            _repository.CreateCommand(commandModel);
+            _repository.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+
+            return CreatedAtRoute(nameof(GetCommandById), new { Id = commandReadDto.Id}, commandReadDto);
+
+            //return Ok(commandReadDto);
+        }
+    }
+}
